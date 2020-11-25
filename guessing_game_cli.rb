33f17394d0b8f08785687b2591aa3ca29348f5b3
Alex Marz <alexhmarz@gmail.com>
2020-11-25 05:37:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,7 +10,7 @@ def run_guessing_game
   if computer_number == user_input
     puts "You guessed the correct number!"
   else 
-    computer_number !== user_input
+    computer_number != user_input
     puts "Sorry! The computer guessed #{computer_number}."
   end
   
